22e50f15ecd3149edbdf6db0809e02bf53f2b01a
SFXy <115570056+SFXy@users.noreply.github.com>
2022-10-16 22:17:49 +0300

    Лабораторная работа №3

---
+++ Лабораторная работа №3/lab_2.py
@@ -0,0 +1,11 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+ind = 0
+max_ = max(set(list_numbers))
+
+for index, num in enumerate(list_numbers):
+    if num == max_:
+        ind = index
+        list_numbers[-1], list_numbers[ind] = list_numbers[ind],list_numbers[-1]
+
+print(list_numbers)
