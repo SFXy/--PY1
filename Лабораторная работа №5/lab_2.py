6b12e29789c0c0114bc52e0719fe9c74aa8ffa5b
SFXy <115570056+SFXy@users.noreply.github.com>
2022-11-15 20:43:17 +0300

    Update lab_2.py

--- Лабораторная работа №5/lab_2.py
+++ Лабораторная работа №5/lab_2.py
@@ -1,12 +1,6 @@
-from random import shuffle
-def get_unique_list_numbers() -> list[int]:
-    list_ = [i for i in range(left_lim, right_lim + 1)]
-    shuffle(list_)
-    return list_[:list_len]  # TODO написать функцию для получения списка уникальных целых чисел
-
-left_lim = -10
-right_lim = 10
-list_len = 15
+from random import sample
+def get_unique_list_numbers(start = -10, stop = 10, size = 15) -> list[int]:
+    return sample([i for i in range(start, stop, 1)], size)
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
